4d5b007e71c3bef094f9837be380e5d3e8716fd0
oliviayixincai <oliviayixincai@gmail.com>
2024-06-02 21:20:29 -0400

    minor change

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ const performFIRECalculation = require('./performFIRECalculation');
 app.use(cors());
 app.use(express.json());
 
-// Test route
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the FIRE Calculator API' });
-});
 
 
 app.post('/calculate', (req, res) => {
@@ -41,3 +37,4 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
